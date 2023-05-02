5210f12f9a89f88d16c37a782d25234800960ef0
Aabid Majid <aabidmajid26@gmail.com>
2023-05-02 20:39:45 +0530

    fix background image etc.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
     const {register, handleSubmit} = useForm();
     return (
         <>
-            <div className='form-group col-lg-6 col-sm-6 col-12' id="form" >
+            <div className='form-group col-lg-6 col-sm-12 col-12' id="form" >
                 <h3 id='form-title'>Hello! Please Fill Out This Form</h3>
                 <form 
                     onSubmit={handleSubmit((data) => {
@@ -14,13 +14,13 @@ export default function Contact() {
 
                 })}>
                     <div className="form-group col-12">
-                        <label>Name: </label><input {...register("name")} className='form-control' type="text" name="name" placeholder="Enter your name" /><br/>
+                        <input {...register("name")} className='form-control' type="text" name="name" placeholder="Enter your name" /><br/>
                     </div>
                     <div className="form-group col-12">
-                        <label>Phone: </label><input {...register("phone")} className='form-control' type="phone" name="phone" placeholder="Enter your mobile number" /><br/>
+                        <input {...register("phone")} className='form-control' type="phone" name="phone" placeholder="Enter your mobile number" /><br/>
                     </div>
                     <div className="form-group col-12">
-                        <label>Email: </label><input {...register("email")} className='form-control' type="email" name="email" placeholder="Enter your email" /><br/>
+                        <input {...register("email")} className='form-control' type="email" name="email" placeholder="Enter your email" /><br/>
                     </div>
                     <button className='btn-danger btn' type='submit'>Submit</button>
                 </form>
